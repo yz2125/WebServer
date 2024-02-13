f17008d728e873503d9f8a02263197d359378bb5
yz2125 <157758526+yz2125@users.noreply.github.com>
2024-02-13 18:07:58 -0500

    Update webServer.py

--- webServer.py
+++ webServer.py
@@ -12,7 +12,7 @@ def webServer(port=13331):
   serverSocket.bind(("", port))
 
   #Fill in start
-  serverSocket.listen(4)
+  serverSocket.listen()
   #Fill in end
 
   while True:
@@ -35,7 +35,7 @@ def webServer(port=13331):
       # Fill in start
       connectionSocket.send(b"HTTP/1.1 200 OK")
       #Content-Type is an example on how to send a header as bytes. There are more!
-      outputdata = b"HTTP/1.1 200 OK\r\n"
+      outputdata += b"HTTP/1.1 200 OK\r\n"
       outputdata += b"Server: SimpleHTTPServer\r\n"
       outputdata += b"Connection: close\r\n"
       outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"
