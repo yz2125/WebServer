8d58e1b68b24d6820a2e502397a92279640dd5dc
yz2125 <157758526+yz2125@users.noreply.github.com>
2024-02-13 18:18:59 -0500

    Update webServer.py

--- webServer.py
+++ webServer.py
@@ -38,7 +38,7 @@ def webServer(port=13331):
       outputdata += b"HTTP/1.1 200 OK\r\n"
       outputdata += b"Server: SimpleHTTPServer\r\n"
       outputdata += b"Connection: close\r\n"
-      outputdata += b"Content-Type: text/html; charset=UTF-8\r\n"
+      outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"
       connectionSocket.sendall(outputdata)
 
       #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
