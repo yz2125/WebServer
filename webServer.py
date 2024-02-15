589f0cb70a3c5bbd82ecc90e215347874790807e
yz2125 <157758526+yz2125@users.noreply.github.com>
2024-02-14 21:43:59 -0500

    Update webServer.py

--- webServer.py
+++ webServer.py
@@ -28,7 +28,6 @@ def webServer(port=13331):
       #opens the client requested file.
       #Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
       f = open(filename[1:], 'r')
-      #f.readlines()
       html_file = ""
       for line in f.readlines():
         html_file += line
